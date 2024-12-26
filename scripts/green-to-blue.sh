29805a9e1bfcfcf7a6cd743ef4bbd4ce402b7f1a
Hasal Chandrasiri <88035046+HasalChandrasiri@users.noreply.github.com>
2024-12-26 11:35:03 +0530

    Update green-to-blue.sh

--- scripts/green-to-blue.sh
+++ scripts/green-to-blue.sh
@@ -1,1 +1,4 @@
 --green-to-blue
+#!/bin/bash
+kubectl apply -f green-deployment.yaml
+kubectl set image deployment/patient-record-service-green patient-record-service=hasaliit/patient-record-service:${GITHUB_SHA} --record
